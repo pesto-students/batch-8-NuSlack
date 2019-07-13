1e60ead139fa0b36a70baf26c294c6c212d30580
saurabh shetty <saurabh.shetty100@gmail.com>
2019-07-13 10:42:55 +0530

    minor fix

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 import { useHomeContext } from '../../context/HomeContext';
 import HomeLayout from '../home-layout';
-import { useHomeContext } from '../../context/HomeContext';
 
 function Home() {
   const { user } = useHomeContext();
