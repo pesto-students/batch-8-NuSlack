3b04ea36fa9a8f412d70021528fd2d610ab0f0cd
Jaspreet Singh <erjaspreet57@gmail.com>
2019-07-17 02:17:49 +0530

    feat(client-socket-module): add socket client in react app
    	* integrate client socket with context module
    
    Resolves: #65

---
+++ client/src/constants/eventNames.js
@@ -0,0 +1,17 @@
+const messageEvent = 'message';
+const connectedUserAckEvent = 'connectedUserAck';
+const connectedUserEvent = 'userConnected';
+const fetchOldChannelMessagesEvent = 'fetchOldChannelMessages';
+const userOnlineEvent = 'userOnline';
+const userDisconnect = 'disconnect';
+const exceptionEvent = 'exception';
+
+export {
+  messageEvent,
+  connectedUserAckEvent,
+  connectedUserEvent,
+  fetchOldChannelMessagesEvent,
+  userOnlineEvent,
+  userDisconnect,
+  exceptionEvent,
+};
